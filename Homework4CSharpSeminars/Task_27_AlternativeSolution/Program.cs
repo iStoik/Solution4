f3a653bcc593aedaf2a465f9032bc065dfebee24
iStoik <vuoksa@bk.ru>
2022-08-06 11:57:09 +0300

    HomeWork4CSharpSeminars

---
+++ Homework4CSharpSeminars/Task_27_AlternativeSolution/Program.cs
@@ -0,0 +1,35 @@
+﻿try
+{
+    Console.Write("Введите целое число: ");
+    int numbers = int.Parse(Console.ReadLine());
+
+    int SumNumbersInNumber(int number)
+    {
+        int sum = 0;
+        int resNum = GetCountNumbers(number);
+        for (int i = 0; i < resNum; i++)
+        {
+            sum = sum + (number % 10);
+            number = number / 10;
+        }
+        return sum;
+    }
+
+    int GetCountNumbers(int num)
+    {
+        int count = 0;
+        while (num != 0)
+        {
+            num = num / 10;
+            count++;
+        }
+        return count;
+    }
+
+    int sumResult = SumNumbersInNumber(numbers);
+    Console.WriteLine("Сумма цифр числа {0} = {1}", numbers, sumResult);
+}
+catch
+{
+    Console.WriteLine("Некорректный ввод!");
+}
---
+++ Homework4CSharpSeminars/Task_29/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Введите размер массива: ");
+string len = Console.ReadLine();
+
+void ExceptionHandling(string enterA)
+{
+    bool yesInt = int.TryParse(enterA, out int result);
+
+    if (yesInt && result > 0)
+    {
+        int[] rndNum = new int[result];
+        FillArray(rndNum);
+        PrintArray(rndNum);
+    }
+    else
+    {
+        Console.WriteLine("{0} -> Некорректный ввод!", len);
+    }
+}
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    string str = "[";
+    foreach (int i in arr)
+    {
+        str += $", {i}";
+    }
+    str = str.Remove(1, 2);
+    str += "]";
+    Console.Write($"Массив из {arr.Length} элементов: {str}");
+}
+
+ExceptionHandling(len);
